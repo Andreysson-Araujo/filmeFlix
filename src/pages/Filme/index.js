a806bc642472578b1ad312896301ddf120cc144f
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2024-10-15 19:43:17 -0500

    iniciando detalhamento

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Filme() {
+  const { id } = useParams();
+
+  useEffect(()=>{
+    async function loadFilme() {
+      await api.get(`/movie/${id}` , {
+        params:{
+          api_key:"35ffe8e8b43516ceb96e92a2b4efcfbe",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        console.log(response.data)
+      })
+      .catch(()=> {
+        console.log("Filme não encontrado")
+      })
+    }
+    loadFilme();
+  }, [])
+
   return(
     <div>
-      <h1>Pagina de detalhes</h1>
+      <h1>Acessando Filme {id}</h1>
     </div>
   )
 }
